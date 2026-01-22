2d34dc28b8699dd0ba5dcb4cc8c46c22b1480bbc
dev-jay-here <jaychapegadikar@gmail.com>
2026-01-22 16:01:40 +0530

    its my first commit

---
+++ task1.py
@@ -0,0 +1,6 @@
+# Purpose of this task:
+# As a developer our first task to check whether it is working or not and that's the reason we are writing our first program as Hello World.
+
+# Prints Hello World.
+
+print("Hello World")
